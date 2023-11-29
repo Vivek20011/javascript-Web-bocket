6b93109c2e897afad6111e573ca9d4e7796c70e9
Vivek20011 <152247287+Vivek20011@users.noreply.github.com>
2023-11-29 04:22:11 +0000

    file added

---
+++ 01basic/test.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+/*
+1.javascript is a high lavel programming language used on developing web &mobile application efficiently .
+2.we use java script in both client side as well as server side language.
+3.we use javasript for developing logics in our code
+4.javascript was developed in 1997.4
+5.now these days we used the es6 version of javascript .it was started on 2015 and now it's the latest version of js.
+esb-echma script 6.(vanilla javascript)
+
+
+*/
