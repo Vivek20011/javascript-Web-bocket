8adebb0c76a5db691c7459eb93d7415baef24ec3
Vivek20011 <152247287+Vivek20011@users.noreply.github.com>
2023-11-30 04:23:09 +0000

    file added

---
+++ 01basic/02datatype.js
@@ -0,0 +1,22 @@
+/*
+1.to be able to operate on variable,it is importantnto know something about the types of data is called datatypes
+2.javascript has 7types datatypes
+number
+bigint
+string
+boolean
+null
+undefiend
+symbol
+*/
+let name="vivek";
+let age=24;
+let ispresent=true;
+let address=null;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof ispresent);
+console.log(typeof address);
+let id=Symbol('123');
+let anotherid=Symbol('123');
+console.log(id===anotherid);
